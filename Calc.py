51fa50afbaadd0516e8f3aa97162e6c0adc32748
vv.yugay <sabot05@mail.ru>
2020-01-30 10:34:56 +0700

    Second commit

--- Calc.py
+++ Calc.py
@@ -1,13 +1,17 @@
 # Calculator ver.1
 
-sign = input("Select action (+, -, *, /)")
+sign = input("Select action (+, -, *, /): ")
 
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
 if sign == "+":
     c = a + b
-    print("result: " + str (c))
 elif sign == "-":
     c = a - b
-    print("result: " + str(c))
+elif sign == "*":
+    c = a * b
+elif sign == "/":
+    c = a / b
+
+print("result: " + str(c))
